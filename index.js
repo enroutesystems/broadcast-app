07403fa77dbdca76f9a688b42a1e2c6200bf946a
Julio Añoveros <julio.ab@outlook.com>
2020-12-30 22:36:44 -0600

    Add style

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, rest) => {
-  res.sendFile('index.html');
-})
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
 
-users = [];
+app.use(express.static(path.join(__dirname, 'public')));
+
+let numUsers = 0;
 
 io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.on('setUserName', (data) => {
-    if(users.indexOf(data) > -1) {
-      users.push(data);
-      socket.emit('userSet', {username: data});
-    } else {
-      socket.emit('userExists', `${data} is taken!, Try some other user name.`);
-    }
-  })
-})
+  let addedUser = false;
 
-http.listen(3000, () => {
-  console.log('listening on localhost:3000');
-})
+  socket.on('new message', (data) => {
+    
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
